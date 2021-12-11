4b53a67438c4d019ae108bcacd2122be05ddae6b
Aaliyah Akora <aaliyahakora@gmail.com>
2021-12-11 23:46:18 +0000

    finished todo adding y and z values to order expect map

--- cube/reorder_test.go
+++ cube/reorder_test.go
@@ -4,12 +4,16 @@ import "testing"
 
 func TestReorder(t *testing.T) {
 
-	// @todo we need to add the Y values
-	// @todo we need to add the Z values
 	var orderExpect = map[Move][7]Cell{
 		Xy: {Zd, Ya, Za, Xd, Xa, Po, Yd},
 		Xz: {Yd, Ya, Za, Xd, Po, Xa, Zd},
 		Xn: {Po, Ya, Za, Xd, Zd, Yd, Xa},
+		Yx: {Xa, Zd, Za, Ya, Yd, Po, Xd},
+		Yz: {Xa, Xd, Za, Po, Yd, Ya, Zd},
+		Yn: {Xa, Po, Za, Zd, Yd, Xd, Ya},
+		Zx: {Xa, Ya, Yd, Za, Po, Zd, Xd},
+		Zy: {Xa, Ya, Xd, Po, Za, Zd, Yd},
+		Zn: {Xa, Ya, Po, Yd, Xd, Zd, Za},
 	}
 
 	for move, expectedOrder := range orderExpect {
